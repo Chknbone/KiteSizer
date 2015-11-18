90c131e0514809b356b4cab7d1f1cab55a45d3e8
Chknbone <cb@palarran.com>
2015-11-18 19:17:51 +1300

    Files

---
+++ test/com/palarran/kitesizer/AllKiteSizerTests.java
@@ -0,0 +1,16 @@
+/* AllKiteSizeTest.java
+ * Dev: CB
+ * 
+ * Test suite for all the JUnit test in the KiteSizer project.
+ */
+package com.palarran.kitesizer;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ WeightTest.class, WindTest.class })
+public class AllKiteSizerTests {
+
+}
